3c3453f299a3794d318479d624cdc49ab53af1fb
filip-cvetkovic <filip.cvetkovic@inmind.rs>
2026-05-13 14:46:32 +0200

    Add BE initial package structure

---
+++ backend/src/main/java/com/example/transactions/dto/TransactionResponse.java
@@ -0,0 +1,4 @@
+package com.example.transactions.dto;
+
+public class TransactionResponse {
+}
---
+++ backend/src/main/java/com/example/transactions/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.example.transactions.exception;
+
+public class GlobalExceptionHandler {
+}
